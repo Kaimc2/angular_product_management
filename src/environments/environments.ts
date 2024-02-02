67c1c0efb9c141df9781fd20014d3109eaf48134
Pichey <picheyprak@gmail.com>
2024-02-02 17:08:09 +0700

    Add files via upload

---
+++ src/environments/environments.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyCyVdOeME9c1ar_73IoUlXdqBJk5Ehr7Tg',
+    authDomain: 'angular-product-managemenet.firebaseapp.com',
+    projectId: 'angular-product-managemenet',
+    storageBucket: 'angular-product-managemenet.appspot.com',
+    messagingSenderId: '66924882931',
+    appId: '1:66924882931:web:06706a282eebb32e0d0086',
+    measurementId: 'G-41Q0DRR73S',
+  },
+};
